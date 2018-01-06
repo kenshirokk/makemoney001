ce88346c28b82bc22a27892a51c5599a03eedc9b
shangbansharen <shangbansharen@163.com>
2018-01-06 19:34:04 +0800

    # 代理实体类

--- src/main/java/com/mtf/admin/entity/Agency.java
+++ src/main/java/com/mtf/admin/entity/Agency.java
@@ -3,6 +3,7 @@ package com.mtf.admin.entity;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 代理表(代理人表/管理员表/管理用户表)
@@ -12,5 +13,21 @@ import java.io.Serializable;
  */
 @Data
 public class Agency implements Serializable {
-    private Integer id;
+
+    private Integer id;             //id 主键
+    private Integer agencyType;     //代理类型
+    private Integer parentId;       //上级代理id
+    private String parentNickname;  //上级代理昵称
+    private String password;        //密码
+    private String nickname;        //昵称
+    private String avatar;          //头像
+    private String phone;           //电话
+    private Integer roomCard;       //房卡
+    private Integer coin;           //金币
+    private Integer agencyBalance;  //代理余额
+    private Integer salaryBalance;  //工资余额
+    private Integer disable;        //禁用
+    private Integer deleted;        //删除
+    private Date createDate;        //创建时间
+    private Integer creatorId;      //创建者id
 }
