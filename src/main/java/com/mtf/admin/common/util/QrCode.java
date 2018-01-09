ad978625c650dc97395c830b3e1212dd147fbff3
shangbansharen <shangbansharen@163.com>
2018-01-09 15:46:42 +0800

    # 二维码生成

--- src/main/java/com/mtf/admin/common/util/QrCode.java
+++ src/main/java/com/mtf/admin/common/util/QrCode.java
@@ -23,9 +23,16 @@ public class QrCode {
 
     private static final String QR_CODE_FOLDER = "qrcode";
     private static final String CHAR_SET = "utf-8";
+    private static final String DEFAULT_FORMAT = "jpg";
+    private static final int DEFAULT_WIDTH = 500;
+    private static final int DEFAULT_HEIGHT = 500;
+    private static final int DEFAULT_WORD_X = 100;
+    private static final int DEFAULT_WORD_Y = 100;
+    private static final int DEFAULT_FONT_SIZE = 30;
 
     /**
      * 生成二维码
+     *
      * @param content
      * @param width
      * @param height
@@ -35,7 +42,8 @@ public class QrCode {
      * @throws WriterException
      * @throws IOException
      */
-    public static Path genQrCode(String content, int width, int height, String format, String fileName) throws WriterException, IOException {
+    public static Path genQrCode(String content, int width, int height, String format, String fileName) throws
+            WriterException, IOException {
         //配置参数
         Map<EncodeHintType, Object> hints = Maps.newHashMap();
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
@@ -50,13 +58,14 @@ public class QrCode {
 
     /**
      * 添加中间logo
+     *
      * @param logoUrl
      * @param path
      * @param format
      * @return
      * @throws IOException
      */
-    public static Path addLogo(String logoUrl, Path path, String format) throws IOException {
+    public static Path addLogo(Path path, String logoUrl, String format) throws IOException {
         BufferedImage qrcode = ImageIO.read(path.toFile());
         BufferedImage logo = ImageIO.read(new URL(logoUrl));
 
@@ -71,5 +80,43 @@ public class QrCode {
         File file = new File(QR_CODE_FOLDER + File.separator + path.getFileName());
         ImageIO.write(qrcode, format, file);
         return Paths.get(file.getPath());
+    }
+
+    /**
+     * 添加文字
+     *
+     * @param path
+     * @param content
+     * @param format
+     * @param x
+     * @param y
+     * @param fontSize
+     * @return
+     * @throws IOException
+     */
+    public static Path addWord(Path path, String content, String format, int x, int y, int fontSize) throws
+            IOException {
+        BufferedImage image = ImageIO.read(path.toFile());
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("微软雅黑", Font.BOLD, fontSize));
+        g.setColor(Color.BLACK);
+        g.drawString(content, x, y);
+        g.dispose();
+        ImageIO.write(image, format, path.toFile());
+        return path;
+    }
+
+    public static Path gen(String qrCodeContent, int qrWidth, int qrHeight, String format, String fileName, String
+            logoUrl, String word, int wordX, int wordY, int fontSize) throws
+            IOException, WriterException {
+        Path path = genQrCode(qrCodeContent, qrWidth, qrHeight, format, fileName);
+        path = addLogo(path, logoUrl, format);
+        return addWord(path, word, format, wordX, wordY, fontSize);
+    }
+
+    public static Path gen(String qrCodeContent, String fileName, String logoUrl, String word) throws
+            IOException, WriterException {
+        return gen(qrCodeContent, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_FORMAT, fileName, logoUrl, word,
+                DEFAULT_WORD_X, DEFAULT_WORD_Y, DEFAULT_FONT_SIZE);
     }
 }
