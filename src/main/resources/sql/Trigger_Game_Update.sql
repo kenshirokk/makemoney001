63a8631601a5426271a47d6efc26aa2ecc6a1385
Tank <tengkangfei@hotmail.com>
2018-02-04 10:54:34 +0800

    游戏设置触发器

---
+++ src/main/resources/sql/Trigger_Game_Update.sql
@@ -0,0 +1,17 @@
+--创建update插入类型触发器
+if (object_id('TGR_Game_Update', 'TR') is not null)
+    drop trigger TGR_Game_Update
+go
+create trigger TGR_Game_Update
+on game_config
+    for update --插入触发
+as
+    --定义变量
+    declare
+		@title varchar(255), --标题
+		@content varchar(255), --内容
+		@desc varchar(255); --描述
+		
+	select @title = param_name , @content = param_value ,@desc = param_desc from inserted;
+	update QPAccountsDB.[dbo].[SystemStatusInfo] set StatusString = @content,StatusTip = @desc where StatusName = @title
+go
